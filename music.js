7fb7abd591bbd657f051c3ef2478d33d5a88556c
paffmaff-cmyk <paffmaff@gmail.com>
2026-03-29 05:44:04 +0300

    Add YouTube cookies support to bypass bot detection

--- music.js
+++ music.js
@@ -270,7 +270,11 @@ class Session {
 
 function createYtDlpResource(url) {
   return new Promise((resolve, reject) => {
-    const ytdlp = spawn(YTDLP_PATH, ['-f', 'bestaudio', '--no-playlist', '-o', '-', '--quiet', url]);
+    const cookiesFile = path.join(__dirname, 'cookies.txt');
+    const ytdlpArgs = ['-f', 'bestaudio', '--no-playlist', '-o', '-', '--quiet'];
+    if (fs.existsSync(cookiesFile)) ytdlpArgs.push('--cookies', cookiesFile);
+    ytdlpArgs.push(url);
+    const ytdlp = spawn(YTDLP_PATH, ytdlpArgs);
     const ffmpeg = spawn(ffmpegPath, [
       '-i', 'pipe:0', '-f', 's16le', '-ar', '48000', '-ac', '2', 'pipe:1',
     ], { stdio: ['pipe', 'pipe', 'ignore'] });
